36b096a42f17113a86c989a47fe2d53b1e51381f
ashwin <ashwin@usistech.com>
2019-09-16 07:22:34 +0530

    Sample navigation pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeNavComponent } from './home-nav/home-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+
+import { AngularMaterialModule } from './angular-material.module';
+import { ApplicationRoutingModule } from './application-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeNavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    AngularMaterialModule,
+    ApplicationRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
